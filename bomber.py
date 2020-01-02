3b2d753e2cf1246054fec8910cf800a029e7e92b
NandyDark <58636468+nandydark@users.noreply.github.com>
2020-01-02 21:58:21 +0530

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -459,7 +459,7 @@ def update():
     stuff_to_update = ['bomber.py', '.version']
     for fl in stuff_to_update:
         dat = urllib.request.urlopen(
-            "https://raw.githubusercontent.com/nandydark/TBOMB-EDITED/master/" + fl).read()
+            "https://raw.githubusercontent.com/nandy121/TBOMB-EDITED/master/" + fl).read()
         file = open(fl, 'wb')
         file.write(dat)
         file.close()
@@ -479,7 +479,7 @@ except Exception:
     exit()
 print('\tChecking For Updates...')
 ver = urllib.request.urlopen(
-    "https://raw.githubusercontent.com/nandydark/TBOMB-EDITED/master/.version").read().decode('utf-8')
+    "https://raw.githubusercontent.com/nandy121/TBOMB-EDITED/master/.version").read().decode('utf-8')
 verl = ''
 try:
     verl = open(".version", 'r').read()
@@ -493,7 +493,7 @@ print("Your Version is Up-To-Date")
 print('\n\n\t\t\tStarting TBomb...\n\n')
 try:
     noti = urllib.request.urlopen(
-        "https://raw.githubusercontent.com/nandydark/TBOMB-EDITED/master/.notify").read().decode('utf-8')
+        "https://raw.githubusercontent.com/nandy121/TBOMB-EDITED/master/.notify").read().decode('utf-8')
     noti = noti.upper().strip()
     if len(noti) > 10:
         print('\n\n\tNOTIFICATION: ' + noti + '\n\n')
